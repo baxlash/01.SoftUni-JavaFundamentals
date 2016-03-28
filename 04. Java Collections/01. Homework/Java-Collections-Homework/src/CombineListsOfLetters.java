d3edecb4367fbd221eeef4b91031cb4f39950e2f
baxlash <yaroslav.dimitrov@outlook.com>
2016-03-28 20:30:14 +0300

    Java-Collection-Homework CombineListOfLetters

--- 04. Java Collections/01. Homework/Java-Collections-Homework/src/CombineListsOfLetters.java
+++ 04. Java Collections/01. Homework/Java-Collections-Homework/src/CombineListsOfLetters.java
@@ -1,42 +1,28 @@
-import com.sun.xml.internal.ws.binding.FeatureListUtil;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Scanner;
 
-/**
- * Created by Baxlash on 3/26/2016.
- */
 public class CombineListsOfLetters {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        String firstLine = console.nextLine().replaceAll(" ","");
-        String secondLine = console.nextLine().replaceAll(" ","");
+        String firstLine = console.nextLine().replaceAll(" ", "");
+        String secondLine = console.nextLine().replaceAll(" ", "");
 
-        List<Character> firstList = new ArrayList<>();
-        List<Character> secondList = new ArrayList<>();
+        System.out.println(combine(firstLine, secondLine));
+    }
 
-        for (int i = 0; i < firstLine.length(); i++) {
-            firstList.add(firstLine.charAt(i));
-        }
-        for (int i = 0; i < secondLine.length(); i++) {
-            secondList.add(secondLine.charAt(i));
-        }
-        List<Character> union = new ArrayList<>(firstList);
+    private static String combine(String s1, String s2) {
 
-        for (int i = firstList.size()-1; i >=0 ; i--) {
-            for (int j = 0; j < secondList.size(); j++) {
-                if(firstList.get(i)==secondList.get(j)){
-                    union.remove(i);
-                }
+        String combined = s1;
+        int count = s2.length();
+        while (count > 0) {
+            String common = s2.substring(0, count);
+            if (s1.endsWith(common)) {
+                combined = s1 + s2.substring(count);
                 break;
             }
-            //continue;
+            count--;
         }
-        union.addAll(secondList);
 
-        System.out.println(union);
-
+        return combined;
     }
+
 }
